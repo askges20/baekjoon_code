e8b0451537b4bbe1b8e558e9507f5f496b21aea8
정예원 <askges20@kyonggi.ac.kr>
2021-06-17 00:09:11 +0900

    Create 1463번 1로 만들기.py
    ,,나는 바보야,,,ㅠㅠ

---
+++ 동적 계획법/1463번 1로 만들기.py
@@ -0,0 +1,10 @@
+n=int(input())
+dp=[0,0]
+for i in range(2,n+1):
+    tmp=[dp[i-1]+1]
+    if i%2==0:
+        tmp.append(dp[i//2]+1)
+    if i%3==0:
+        tmp.append(dp[i//3]+1)
+    dp.append(min(tmp))
+print(dp[n])
